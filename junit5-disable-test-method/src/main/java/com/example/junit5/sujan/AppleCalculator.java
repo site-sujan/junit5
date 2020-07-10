34eed07b46a7fcba8a33ace2b3e4e8fc6f13c961
shrikant sharma <shrikant.sharma606@gmail.com>
2020-07-11 00:41:38 +0530

    examples

---
+++ junit5-disable-test-method/src/main/java/com/example/junit5/sujan/AppleCalculator.java
@@ -0,0 +1,11 @@
+package com.example.junit5.sujan;
+
+public class AppleCalculator {
+    public int addApple(int apple1, int apple2) {
+        return apple1 + apple2;
+    }
+
+    public int subApple(int apple1, int apple2) {
+        return apple1 * apple2;
+    }
+}
